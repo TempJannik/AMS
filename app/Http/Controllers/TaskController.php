d9465d5b08e734fd395ac28446febd8f2baf0db3
Jannik P <30756486+TempJannik@users.noreply.github.com>
2023-12-12 21:15:43 +0100

    Created Task Model
    Created Model, Controller, Seeder and Route for the Task Model.

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Validation\Rule;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Task $taskl)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'status' => ['required', Rule::in(['todo', 'in_progress', 'done'])],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, int $id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, int $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'description' => 'required',
+            'status' => ['required', Rule::in(['todo', 'in_progress', 'done'])],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, int $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json([], 204);
+    }
+}

5fdc9854b55d68af7d07c1a8e9f1d601d22e19af
Jannik P <30756486+TempJannik@users.noreply.github.com>
2023-12-15 16:37:11 +0100

    Added tests for new features
    Added tests for new routes, new authorization cases, relationships and performance

---
+++ tests/Feature/RelationshipTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Database\Eloquent\Collection;
+use Tests\TestCase;
+use App\Models\Task;
+use App\Models\User;
+use App\Models\Project;
+
+class RelationshipTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_task_user_relationship()
+    {
+        $user = User::factory()->create();
+        $task = Task::factory()->create(['user_id' => $user->id]);
+
+        $reloadedTask = Task::with('user')->find($task->id);
+
+        $this->assertTrue($reloadedTask->user instanceof User);
+        $this->assertEquals($user->id, $reloadedTask->user->id);
+    }
+
+    public function test_task_project_relationship()
+    {
+        $project = Project::factory()->create();
+        $task = Task::factory()->create(['project_id' => $project->id]);
+
+        $reloadedTask = Task::with('project')->find($task->id);
+
+        $this->assertTrue($reloadedTask->project instanceof Project);
+        $this->assertEquals($project->id, $reloadedTask->project->id);
+    }
+
+    public function test_user_tasks_relationship()
+    {
+        $user = User::factory()->create();
+        $tasks = Task::factory()->count(3)->create(['user_id' => $user->id]);
+
+        $reloadedUser = User::with('tasks')->find($user->id);
+
+        $this->assertTrue($reloadedUser->tasks instanceof Collection);
+        $this->assertCount(3, $reloadedUser->tasks);
+        $this->assertEquals($tasks->pluck('id'), $reloadedUser->tasks->pluck('id'));
+    }
+
+    public function test_project_tasks_relationship()
+    {
+        $project = Project::factory()->create();
+        $tasks = Task::factory()->count(3)->create(['project_id' => $project->id]);
+
+        $reloadedProject = Project::with('tasks')->find($project->id);
+
+        $this->assertTrue($reloadedProject->tasks instanceof Collection);
+        $this->assertCount(3, $reloadedProject->tasks);
+        $this->assertEquals($tasks->pluck('id'), $reloadedProject->tasks->pluck('id'));
+    }
+}
--- tests/Feature/TaskAuthenticationApiTest.php
+++ tests/Feature/TaskAuthenticationApiTest.php
@@ -44,6 +44,17 @@ class TaskAuthenticationApiTest extends TestCase
         $this->assertDatabaseHas('tasks', $taskToFind->toArray());
     }
 
+    public function test_cant_delete_other_users_task(): void
+    {
+        $task = Task::factory()->create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->delete("/api/tasks/{$task->id}", [], ['Accept' => 'application/json']);
+        $response->assertStatus(403);
+    }
+
     public function test_cant_create_task(): void
     {
         $taskToCreate = [
@@ -56,10 +67,29 @@ class TaskAuthenticationApiTest extends TestCase
         $this->assertDatabaseMissing('tasks', $taskToCreate);
     }
 
-    public function test_can_update_task(): void
+    public function test_cant_update_other_users_task(): void
     {
-        $taskToFind = Task::first();
-        $this->assertNotNull($taskToFind);
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+        $task = Task::factory()->create(['user_id' => $otherUser]);
+
+        $this->actingAs($user);
+
+        $updatedData = [
+            'title' => 'Aktualisierter Titel',
+            'description' => 'Neue Beschreibung',
+            'status' => 'in_progress',
+        ];
+
+        $response = $this->put("/api/tasks/{$task->id}", $updatedData, ['Accept' => 'application/json']);
+        $response->assertStatus(403);
+    }
+
+    public function test_cant_update_overdue_task(): void
+    {
+        $task = Task::factory()->create([
+            'deadline' => now()->subDay(),
+        ]);
 
         $updatedData = [
             'title' => 'Neuer Titel',
@@ -67,8 +97,29 @@ class TaskAuthenticationApiTest extends TestCase
             'status' => 'done',
         ];
 
-        $response = $this->put("/api/tasks/{$taskToFind->id}", $updatedData, ['Accept' => 'application/json']);
-        $response->assertStatus(401);
-        $this->assertDatabaseHas('tasks', $taskToFind->toArray());
+        $this->actingAs($task->user);
+        $response = $this->put("/api/tasks/{$task->id}", $updatedData, ['Accept' => 'application/json']);
+        $response->assertStatus(403);
+    }
+
+    public function test_admin_can_update_overdue_task(): void
+    {
+        $adminUser = User::where('name', 'Admin User')->first();
+        $this->assertNotNull($adminUser);
+        $task = Task::factory()->create([
+            'deadline' => now()->subDay(),
+            'user_id' => $adminUser
+        ]);
+        $this->assertNotNull($task);
+
+        $updatedData = [
+            'title' => 'Neuer Titel',
+            'description' => 'Neue Beschreibung',
+            'status' => 'done',
+        ];
+
+        $this->actingAs($adminUser);
+        $response = $this->put("/api/tasks/{$task->id}", $updatedData, ['Accept' => 'application/json']);
+        $response->assertStatus(200);
     }
 }
